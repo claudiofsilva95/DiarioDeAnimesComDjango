4ad11e4c3b2248ead952457b646f8b0bfae44c44
Claudio Silva <79285332+claudiofsilva95@users.noreply.github.com>
2021-06-18 13:13:57 -0300

    Update models.py

--- animes/models.py
+++ animes/models.py
@@ -9,7 +9,7 @@ class Anime(models.Model):
     eps_assistidos = models.CharField(max_length=10000)
     estado = models.CharField(max_length=100)
     nota = models.CharField(max_length=100)
-    foto = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True, null=True)
+    foto = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
 
     def __str__(self):
         return self.nome_anime
